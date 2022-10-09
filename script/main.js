3beae61d0066e303134e3b611ff1714a89b4c7a2
Eden Abraham <eabraham@atl.yearup.org>
2022-10-09 01:00:53 -0400

    made 2 pgs, connected them with nav, +2 css+2js

---
+++ script/main.js
@@ -0,0 +1,10 @@
+/* 
+//old school init works only if its the only listener to onload event
+function init() {
+    
+}
+window.onload= init // wait until body/html is loaded
+*/
+document.addEventListener("DOMContentLoaded",()=>( // begin annonymous
+//init code here
+)); 
